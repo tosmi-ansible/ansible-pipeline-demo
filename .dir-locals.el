1afef7c084eefd9b6db25da27f79be3452d33c04
Toni Schmidbauer <toni@stderr.at>
2023-10-07 13:37:08 +0200

    import emacs dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((eval . (conda-env-activate "ansible"))))
+ (yaml-mode . ((flycheck-checker . ansilbe-ansiblelint))))
---
+++ roles/controller/tasks/.dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((yaml-mode . t)
+	 (mode . ansible-doc)
+	 (mode . yaml-pro))
+ (yaml-mode (eval add-hook 'yaml-mode-hook '(lambda () (ansible 1)))))
