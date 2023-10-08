d2265f3fcee8fdfaca21e77812f4dcf2856377fb
Toni Schmidbauer <toni@stderr.at>
2023-10-08 21:22:00 +0200

    remove conda active, makes everything slow

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((eval . (conda-env-activate "ansible"))))
- (yaml-mode . ((flycheck-checker . ansilbe-ansiblelint))))
+((yaml-mode . ((flycheck-checker . ansilbe-ansiblelint))))
